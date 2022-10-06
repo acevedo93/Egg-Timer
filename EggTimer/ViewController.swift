3311f2c6216042be1f73d424fa6a7f6d7be496f9
David Acevedo <davidacevedo@Davids-MacBook-Pro.local>
2022-10-06 07:42:53 -0500

    initial commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,15 +1,55 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var softTime:Int = 5
+    var mediumTime:Int = 7
+    var hardTime:Int = 12
+    var totalTime = 0
+    var secondsPassed = 0
+    var timer = Timer()
+    var player : AVAudioPlayer!
     
-
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        titleLabel.text = sender.currentTitle
+        timer.invalidate()
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        totalTime = checkHardnessTime(hardness: sender.currentTitle!)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer () {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            let calculate = Float(secondsPassed) / Float(totalTime)
+            self.progressBar.progress =  calculate
+        } else {
+            timer.invalidate()
+            self.titleLabel.text = "Done"
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.setVolume(0.2, fadeDuration: 0.2)
+            player.play()
+        }
+    }
+    
+    
+    func checkHardnessTime (hardness:String) -> Int {
+        let eggTimes: [String : Int] = [
+            "Soft": softTime,
+            "Medium": mediumTime,
+            "Hard": hardTime
+        ]
+        
+        if let time = eggTimes[hardness] {
+            return time
+        } else {
+            return 0
+        }
+    }
 }
